de378ace9294d6d0ea8022b93223974c2d19e13f
Ryan Chan <tengc.chan@gmail.com>
2020-12-27 16:29:09 +0800

    [Feat] Every hour at :15, :35 run auto_test

--- speed_record.py
+++ speed_record.py
@@ -11,17 +11,31 @@ import schedule
 
 csv_file_name = "ouput.csv"
 excel_file_name = "output.xlsx"
-speedtest_out = "speedtest_out"
+speedtest_file_name = "speedtest_out"
 
-#def parse_speedtest_file(file_path):
-     
+def parse_speedtest_meta(start_time, finish_time, file_path):
+    fp = open(file_path, 'r')  
+    lines = fp.readlines()
 
-def speed_test_func():
-    time = datetime.now()
-    print("speed-test beginning ... {}".format(time))
-    os.system("/home/pi/auto_speed_test/speedtest-cli > " + speedtest_out)
-    time = datetime.now()
-    print("speed-test done {:d}".format(time))
+    provider = (lines[1].split(" from ")[1]).split("...")[0]
+    server = (lines[4].split(" by ")[1]).split(": ")[0]
+    ping = ((lines[4].split(" by ")[1]).split(": ")[1]).split(" ")[0]
+    dl_speed = lines[6].split(" ")[1]
+    ul_speed = lines[8].split(" ")[1]
+
+    meta = []
+    meta.append(start_time)
+    meta.append(finish_time)
+    meta.append(ping)
+    meta.append(dl_speed)
+    meta.append(ul_speed)
+    meta.append(provider)
+    meta.append(server)
+    #print(meta)
+    return meta
+
+def run_speedtest():
+    os.system("/home/pi/auto_speed_test/speedtest-cli > " + speedtest_file_name)
    
 def csv_write_meta():
     with open(csv_file_name, 'a', newline='') as csv_file:
@@ -30,8 +44,10 @@ def csv_write_meta():
         fwrite.writerow(['a1', 'b1', 'c1'])
         fwrite.writerow(['2a', 'b', 'c'])
 
-def excel_write_meta():
+def excel_write_meta(meta):
 
+    column_idx = 1
+    row_idx = 1
     now = datetime.now()
     date_str = now.strftime("%Y%m%d")
 
@@ -39,39 +55,50 @@ def excel_write_meta():
         wb = openpyxl.load_workbook(excel_file_name)
         if date_str in wb.sheetnames:
             write_sheet = wb[date_str]
-            new_row = write_sheet.max_row + 1
-            write_sheet['A' + str(new_row)] = now.strftime("%Y_%m_%d_%H_%M_%S")
+            row_idx = write_sheet.max_row + 1
         else:
             wb = openpyxl.Workbook()
             wb.create_sheet(date_str)
             write_sheet = wb[date_str]
-            write_sheet['A1'] = now.strftime("%Y_%m_%d_%H_%M_%S")
     else:
         wb = openpyxl.Workbook()
         wb.create_sheet(date_str)
         write_sheet = wb[date_str]
-        write_sheet['A1'] = now.strftime("%Y_%m_%d_%H_%M_%S")
+ 
+    #write_sheet['A' + str(new_row)] = now.strftime("%Y_%m_%d_%H_%M_%S")
+
+    for data in meta:
+        write_sheet.cell(row=row_idx, column=column_idx).value = data
+        column_idx += 1
+
+    #write_sheet.cell(row=10, column=1).value = "ryan_test"
 
     wb.save(excel_file_name)
 
+def __task():
+    start_time = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+    print("speed-test beginning ... %s" % start_time)
+    run_speedtest()
+    finish_time = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+    print("speed-test done ... %s" % finish_time)
+    meta = parse_speedtest_meta(start_time, finish_time, speedtest_file_name)
+    excel_write_meta(meta)
+
 def main():
     print("__ start ++")
-    csv_write_meta()
-    excel_write_meta()
 
-    now = datetime.now()
-    date_str = now.strftime("%Y%m%d")
-    print("%s" % date_str)
-    #timer_func()
+    #schedule.every(2).minutes.do(__task)
+    #schedule.every(5).seconds.do(__task)
+    #schedule.every().day.at('09:30').do(job1)
+    schedule.every().hour.at(":05").do(__task)
+    schedule.every().hour.at(":20").do(__task)
+    schedule.every().hour.at(":35").do(__task)
+    schedule.every().hour.at(":50").do(__task)
 
-    schedule.every(1).minute.do(speed_test_func)
-
-    '''while True:
+    while True:
         schedule.run_pending()
-        time.sleep(1)'''
+        time.sleep(1)
 
-    
-    speed_test_func()
 
 if __name__ == "__main__":
     main()
